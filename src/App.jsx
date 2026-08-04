6ecf6ad3da513c638f103322e654fad298e6f878
Tribikram11 <22btaml33@suiit.ac.in>
2026-08-04 13:29:59 +0530

    feat: learnt context API and implement theme context and add theme switch functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
 
-import FocusableForm from "./components/FocusableForm"
-
+import { createContext, useContext, useState } from "react"
+const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState('light')
+
   return (<>
+    <ThemeContext.Provider value={{theme, setTheme}}>
+    <HeaderDisplay/>
+    
+    <Page/>
+    </ThemeContext.Provider>
   
-  <FocusableForm/>
   </>
   )
+}
+
+function HeaderDisplay() {
+  const { theme } = useContext(ThemeContext);
+  return <h1>App Theme: {theme}</h1>;
+}
+
+export function Page(){
+    return(<>
+    <h2>page component</h2>
+    <ToolBar/>
+    </>)
+}
+
+export function ToolBar(){
+  return(<>
+    <h2>toolbar component</h2>
+    <ThemeSwitchButton/>
+  </>)
+}
+
+export function ThemeSwitchButton(){
+  const {theme, setTheme} = useContext(ThemeContext);
+
+  const handleClick = () => {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'))
+  }
+
+  return(<>
+    <button onClick={handleClick}>change </button>
+  </>)
 }
 
 export default App
