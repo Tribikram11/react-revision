c3f7bbaf5a16dd75bdfd8551e1fe60bd3ee004a7
bikram <22btaml33@suiit.ac.in>
2026-09-21 12:17:03 +0530

    feat: add CartSummary component for displaying cart details

--- mini-project-2/src/App.jsx
+++ mini-project-2/src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { CartProvider } from './context/CartContext';
 import ProductList from './component/ProductList';
-// import CartSummary from './components/CartSummary';
+import CartSummary from './component/CartSummary';
+
 
 export default function App() {
   return (
     <CartProvider>
       <div>
-        <h1>Live Product Search & Cart</h1>
+        <h1>Product Search & Cart</h1>
         
         <div >
           <ProductList />
-          {/* <CartSummary /> */}
+          <CartSummary/>
         </div>
       </div>
     </CartProvider>
---
+++ mini-project-2/src/component/CartSummary.jsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+
+function CartSummary() {
+  const { cart } = useContext(CartContext);
+
+  const totalPrice = cart.reduce((acc, item) => acc + item.price , 0);
+  return (<>
+    <h2>cart summary</h2>
+    <p>total items: {cart.length}</p>
+    <p>total price: {totalPrice}</p>
+
+    {cart.length > 0 && (
+        <ul>
+            {cart.map((item, index) => {
+                <li key={index}>
+                    {item.name} (${item.price})
+                </li>
+            })}
+        </ul>
+    )}
+  </>);
+}
+
+export default CartSummary;
